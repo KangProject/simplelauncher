4722270861508b710200fedab6ee9d210f63cf8e
Stephen A. Gutknecht <stephen@hilltx.com>
2014-09-20 12:28:15 -0500

    Added Web Server from Android SDK sample

--- SimpleLauncher/src/main/java/ch/arnab/simplelauncher/HomeScreenDirectLaunch.java
+++ SimpleLauncher/src/main/java/ch/arnab/simplelauncher/HomeScreenDirectLaunch.java
@@ -1,16 +1,28 @@
 package ch.arnab.simplelauncher;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
+import org.chrome.remote.JavaWebServer;
+import org.chrome.remote.JavaWebServerService;
+
 public class HomeScreenDirectLaunch extends FragmentActivity {
+
+    public static JavaWebServer myWebServer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
+
+        if (JavaWebServerService.mySelf == null)
+        {
+            Intent remoteWebServerIntent = new Intent(this, JavaWebServerService.class);
+            getApplicationContext().startService(remoteWebServerIntent);
+        }
     }
 
 
